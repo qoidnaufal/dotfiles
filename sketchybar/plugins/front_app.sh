de21dc8254ff72ea1cbc7f1b7c3ed612e974e168
qoidnaufal <qoidnaufal@gmail.com>
2025-06-05 10:31:46 +0700

    ...

--- sketchybar/plugins/front_app.sh
+++ sketchybar/plugins/front_app.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+source "$CONFIG_DIR/tokens/cha_colors.sh"
+
 if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" icon.background.image="app.$INFO" icon.background.image.scale=0.8
+  sketchybar --set "$NAME" label="$INFO" \
+            background.color=$CHA_COLOR_BG1_75 \
+        		background.corner_radius=20 \
+        		background.height=24 \
+            label.padding_left=8 \
+            label.padding_right=8 \
+            icon.background.image="app.$INFO" \
+            icon.background.image.padding_left=8 \
+            icon.background.image.scale=0.6
 fi
